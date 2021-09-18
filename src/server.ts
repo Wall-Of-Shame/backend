346a72f55a9c494394cd2af32c981900ec7b74f9
jiefeng <lowjiefeng1998@gmail.com>
2021-09-18 13:19:54 +0800

    Add firebase-admin

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import cors, { CorsOptions } from "cors";
 import express from "express";
+import admin from "firebase-admin";
 import helmet from "helmet";
 import { Server } from "http";
 import morgan from "morgan";
+
 import routes from "./routes";
 
 const API_VERSION = "/api/v1";
@@ -10,10 +12,20 @@ const API_VERSION = "/api/v1";
 const corsOptions: CorsOptions = {
   origin: "*",
 };
+
+function verifyEnvOrReject(): void {
+  const { FIREBASE_DB_URL, GOOGLE_APPLICATION_CREDENTIALS } = process.env;
+
+  if (!FIREBASE_DB_URL || !GOOGLE_APPLICATION_CREDENTIALS) {
+    throw new Error("Environment was not configured properly.");
+  }
+}
 export class ApiServer {
   server: Server | undefined;
 
   async initialize(port = 3000): Promise<void> {
+    verifyEnvOrReject();
+
     const app = express();
     app.use(express.json({ limit: "20mb" }));
     app.use(express.urlencoded({ extended: true, limit: "20mb" }));
@@ -21,6 +33,11 @@ export class ApiServer {
     app.use(helmet());
     app.use(morgan("dev"));
     app.use(API_VERSION, routes);
+
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
+      databaseURL: `https://${process.env.FIREBASE_DB_URL}.firebaseio.com`,
+    });
 
     const message =
       `Server connected successfully on port: ${port}.\n` +
